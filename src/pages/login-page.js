b256562b26b91790a7dce573401ab77958799b00
shivam <shivam.spirite3@outlook.com>
2022-07-12 03:05:15 +0530

    add env files

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -4,6 +4,7 @@ import Login from '../components/auth/login/login'
 import '../assest/css/pages.css'
 
 function LoginPage() {
+  console.log('kelji')
   return (
     <div className='sectionContainer'>
       <Header/>
